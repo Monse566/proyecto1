8511324918a935e5523ec1aac0e32da54fb04b18
Monse566 <alejandramvc666@gmail.com>
2024-03-24 21:02:11 -0600

    class libro

--- scripts.js
+++ scripts.js
@@ -14,3 +14,39 @@ alfabéticamente
 Crea una función que pida un género y muestre la información
 de los libros que pertenezcan a ese género usando un el método
 que devuelve la información*/
+
+class Book {
+  constructor(title, author, year, gender) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+    this.gender = gender;
+  }
+
+  bookInfo() {
+    return `${this.title} es un libro de ${this.gender} escrito por ${this.author} en el año ${this.year}`;
+  }
+}
+
+let books = [];
+
+while (books.length < 3) {
+  let title = prompt("Introduce el titulo del libro");
+  let author = prompt("Introduce el autor del libro");
+  let year = prompt("Introduce el año del libro");
+  let gender = prompt("Introduce el genero del libro").toLocaleLowerCase();
+
+  if (
+    title != "" &&
+    author != "" &&
+    !isNaN(year) &&
+    year.length == 4 &&
+    (gender == "aventura" || gender == "terror" || gender == "fantasia")
+  ) {
+    books.push(new Book(title, author, year, gender));
+  }
+}
+
+const showAllBooks = () => {
+  console.log(books);
+};
